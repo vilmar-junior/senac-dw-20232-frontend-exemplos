ccd28a11e7c95e1f3d2189b3905e182dfc7967b3
Vilmar César Pereira Júnior <41882583+vilmar-junior@users.noreply.github.com>
2023-07-27 00:08:19 -0300

    Add files via upload

---
+++ js/exemplos.js
@@ -0,0 +1,7 @@
+let prompt = require('prompt-sync')();
+
+let curso = prompt('Qual o nome do seu curso?');
+console.log('Curso: ' + curso);
+
+let fase = Number(prompt('Informe a fase: '));
+console.log('Fase: ' + fase);
