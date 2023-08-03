e432522d07d6a71763ea768276e6f3dc51c151c7
Vilmar C. Pereira Júnior <vilmarcesarpereira@gmail.com>
2023-08-03 11:11:20 -0300

    Integração Javascript e HTML

--- js/consultaviacep.js
+++ js/consultaviacep.js
@@ -1,45 +1,52 @@
-/////////   Versão 1: chamadas direto em Javascript /////////
-//URL de teste: https://viacep.com.br/ws/88495000/json/
-//Referência:https://viacep.com.br/modulos_e_pacotes/
-// //1- instanciar o prompt
-// let prompt = require('prompt-sync')();
-
-// //2- solicitar um CEP para o usuário no terminal
-// let cepInformado = prompt('Informe o CEP:');
-
-// //3- Chamar a função de consulta de cep
-// buscarCEP(cepInformado);
-
-// //Função assíncrona (não sabemos quando retorna)
-// async function buscarCEP(cep){
-//     //Parâmetros opcionais para a requisição
-//     let options = {
-//         method: "GET",
-//         headers: {"Content-type": "application/json"}
-//     };
-//     //Esta variável é uma Promise (um objeto que será preenchido quando a requisição HTTP retornar)
-//     //Atenção com as aspas, para passar o parâmetro CEP devemos usar o ACENTO GRAVE
-//     const promiseConsultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`, options);
-
-//     //Retorno da Promise
-//     const json = await promiseConsultaCEP.json();
-// }
-///////////////////////////////////////////////////////////////
-
 /////////   Versão 2: para chamar diretamente do HTML /////////
 //O valor do cep digitado está no <input> com id "txtCep"
 async function buscarCEP(){
+    limpar();
     //'document' é uma variável global que representa todo o HTML e seus elementos (a árvore DOM - Document Object Model)
-
     var txtCep = document.getElementById('txtCep');
     var cepInformado = txtCep.value;
 
-    const promiseConsultaCEP = await fetch(`https://viacep.com.br/ws/${cepInformado}/json/`);
+    fetch(`https://viacep.com.br/ws/${cepInformado}/json/`)
+    .then(resultado => resultado.json())
+    .then(json => {
+        if(json.erro){
+            mostrarTelaErro();
+        }else{
+            preencherCamposComJSON(json);
+        }
+    })
+    .catch(erro => {
+        mostrarTelaErro();
+    })
+}
 
-    //Retorno da Promise
-    const json = await promiseConsultaCEP.json();
-    console.log(json);
+//Preencher os dados do endereço obtido na página HTML
+function preencherCamposComJSON(json){
+    //essa condição funciona em javascript, é o equivalente a 
+    //if(json.bairro != undefined && json.bairro != '')
+    if(json.bairro){ 
+        //Obter o componente diretamente pelo id funciona (não sabia)
+        txtBairro.value = json.bairro;
+    }else{
+        txtBairro.disabled = false;
+    }
 
-    //TODO preencher os dados do endereço obtido na página HTML
+    //Versão 2 (mais antiga): obter o componente navegando na árvore DOM
+    document.getElementById('txtUF').value = json.uf;
+    txtCidade.value = json.localidade;
+}
+
+function limpar(){
+    divDadosEndereco.style = 'background-color: aqua';
+    txtBairro.value = '';
+    txtCidade.value = '';
+    txtUF.value = '';
+    txtBairro.disabled = true;
+}
+
+function mostrarTelaErro(){
+    limpar();
+    divDadosEndereco.style = 'background-color: red';
+    alert('CEP informado não existe');
 }
 ///////////////////////////////////////////////////////////////
